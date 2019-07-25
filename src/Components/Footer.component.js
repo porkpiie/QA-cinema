564b319733324b8e153a8680fa60b34e49df5801
porkpiie <adtready@gmail.com>
2019-07-25 16:53:34 +0100

    added some extra bits

--- src/Components/Footer.component.js
+++ src/Components/Footer.component.js
@@ -4,8 +4,8 @@ import { Navbar, Container, NavbarBrand } from 'reactstrap';
 export default class AppFooter extends Component{
     render() {
         return(
-            <div className="fixed-bottom">  
-                <Navbar color="dark" dark>
+            <div className="fixed-bottom navbar-dark bg-company-black">  
+                <Navbar>
                     <Container>
                         <NavbarBrand style={{color: 'white', textDecoration: 'none'}}>&copy; QA Cinemas 2019</NavbarBrand>
                     </Container>
--- src/Components/Navbar.component.js
+++ src/Components/Navbar.component.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Nav, NavItem, Dropdown, DropdownItem, DropdownToggle, DropdownMenu, NavLink } from 'reactstrap';
+import { Button, Nav, NavItem, Dropdown, DropdownItem, DropdownToggle, DropdownMenu, Form, NavLink } from 'reactstrap';
+import { FormControl} from 'react-bootstrap';
+
 
 export default class Navigation extends Component {
   constructor(props) {
@@ -25,36 +27,40 @@ export default class Navigation extends Component {
               <img src="http://www.apprenticeshipguide.co.uk/wp-content/uploads/2018/01/QA-logo-2019-3-300x300.jpg" width="50" height="50" alt="qa.com" />
             </a>
           <NavItem>
-            <NavLink href="/home" style={{color: 'white', textDecoration: 'none'}}>Home</NavLink>
+            <NavLink href="/home">Home</NavLink>
           </NavItem>
           
           <NavItem>
-            <NavLink href="/WhatsOn" style={{color: 'white', textDecoration: 'none'}}>What's On?</NavLink>
+            <NavLink href="/WhatsOn">What's On?</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/ComingUp" style={{color: 'white', textDecoration: 'none'}}>What's Coming Up?</NavLink>
+            <NavLink href="/ComingUp">What's Coming Up?</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/Booking" style={{color: 'white', textDecoration: 'none'}}>Book Now!</NavLink>
+            <NavLink href="/Booking">Book Now!</NavLink>
           </NavItem>
           <Dropdown nav isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-            <DropdownToggle nav caret style={{color: 'white', textDecoration: 'none'}}>
+            <DropdownToggle nav caret>
               More
             </DropdownToggle>
-            <DropdownMenu>
+            <DropdownMenu style={{backgroundColor : 'black'}}>
               <DropdownItem header>About The Cinema</DropdownItem>
-              <DropdownItem href="/Screens">Our Screens </DropdownItem>
-              <DropdownItem href="/AboutUs">About Us</DropdownItem>
-              <DropdownItem href="/Classifications">Classifications</DropdownItem>
+              <DropdownItem href="/Screens" style={{color: 'red', textDecoration: 'none'}}>Our Screens </DropdownItem>
+              <DropdownItem href="/AboutUs" style={{color: 'red', textDecoration: 'none'}}>About Us</DropdownItem>
+              <DropdownItem href="/Classifications" style={{color: 'red', textDecoration: 'none'}}>Classifications</DropdownItem>
               <DropdownItem divider />
               <DropdownItem header>What's Nearby?</DropdownItem>
-              <DropdownItem href="/GettingHere">Getting Here</DropdownItem>
-              <DropdownItem href="/ThingsToDo">Things To Do</DropdownItem>
+              <DropdownItem href="/GettingHere" style={{color: 'red', textDecoration: 'none'}}>Getting Here</DropdownItem>
+              <DropdownItem href="/ThingsToDo" style={{color: 'red', textDecoration: 'none'}}>Things To Do</DropdownItem>
             </DropdownMenu>
           </Dropdown>
           <NavItem>
             <NavLink disabled href="#">Watch this space</NavLink>
           </NavItem>
+          <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
         </Nav>
       </div>
     );
