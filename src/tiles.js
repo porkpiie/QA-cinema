62274b2d5ea57357d968ba52ed555a2c33cb6a33
ioawnen <ioawnen@gmail.com>
2019-07-29 16:39:54 +0100

    Updates tiles.js to use state to geenrate tiles

--- src/tiles.js
+++ src/tiles.js
@@ -5,71 +5,45 @@ import { MDBMask, MDBView, MDBContainer, MDBRow, MDBCol } from 'mdbreact';
 
 export default class Tiles extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            movies: [
+                { title: 'The Lion King', image: image1 },
+                { title: 'The Lion King', image: image1 },
+                { title: 'The Lion King', image: image1 },
+                { title: 'The Lion King', image: image1 }
+                
+            ]
+        }
+    }
+
     render() {
         return (
             <div>
                 <MDBContainer>
                     <MDBRow>
-                        <MDBCol>
-                            <MDBView hover>
-
-                                <Card bg="dark" text="white" style={{ width: '10rem' }}>
-                                    <Card.Img variant="top" src={image1} />
-                                    <Card.Body className="p-2 pt-4">
-                                        <Card.Title style={{ textAlign: "left", fontSize: 10 }}>THE LION KING</Card.Title>
-                                    </Card.Body>
-                                </Card>
-                                <MDBMask style={{ width: '10rem', height: '15rem', fontSize: 15 }} overlay="black-strong">
-                                    <p className="white-text p-2 pt-10">DIRECTOR:<br />CAST:<br />RUNTIME:</p>
-                                </MDBMask>
-                            </MDBView>
-                        </MDBCol>
-
-                        <MDBCol>
-                            <MDBView hover>
-                                <Card bg="dark" text="white" style={{ width: '10rem' }}>
-                                    <Card.Img variant="top" src={image1} />
-                                    <Card.Body className="p-2 pt-4">
-                                        <Card.Title style={{ textAlign: "left", fontSize: 10 }}>THE LION KING</Card.Title>
-                                    </Card.Body>
-                                </Card>
-                                <MDBMask style={{ width: '10rem', height: '15rem', fontSize: 15 }} overlay="black-strong">
-                                    <p className="white-text p-2 pt-10">DIRECTOR:<br />CAST:<br />RUNTIME:</p>
-                                </MDBMask>
-                            </MDBView>
-                        </MDBCol>
-
-                        <MDBCol>
-                            <MDBView hover>
-                                <Card bg="dark" text="white" style={{ width: '10rem' }}>
-                                    <Card.Img variant="top" src={image1} />
-                                    <Card.Body className="p-2 pt-4">
-                                        <Card.Title style={{ textAlign: "left", fontSize: 10 }}>THE LION KING</Card.Title>
-                                    </Card.Body>
-                                </Card>
-                                <MDBMask style={{ width: '10rem', height: '15rem', fontSize: 15 }} overlay="black-strong">
-                                    <p className="white-text p-2 pt-10">DIRECTOR:<br />CAST:<br />RUNTIME:</p>
-                                </MDBMask>
-                            </MDBView>
-                        </MDBCol>
-
-                        <MDBCol>
-                            <MDBView hover>
-                                <Card bg="dark" text="white" style={{ width: '10rem' }}>
-                                    <Card.Img variant="top" src={image1} />
-                                    <Card.Body className="p-2 pt-4">
-                                        <Card.Title style={{ textAlign: "left", fontSize: 10 }}>THE LION KING</Card.Title>
-                                    </Card.Body>
-                                </Card>
-                                <MDBMask style={{ width: '10rem', height: '15rem', fontSize: 15 }} overlay="black-strong">
-                                    <p className="white-text p-2 pt-10">DIRECTOR:<br />CAST:<br />RUNTIME:</p>
-                                </MDBMask>
-                            </MDBView>
-                        </MDBCol>
+                        {this.state.movies.map(movie => (
+                            <MDBCol>
+                                <MDBView hover>
+                                    <Card bg="dark" text="white" style={{ width: '10rem' }}>
+                                        <Card.Img variant="top" src={movie.image} />
+                                        <Card.Body className="p-2 pt-4">
+                                            <Card.Title style={{ textAlign: "left", fontSize: 10 }}>
+                                                {movie.title.toLocaleUpperCase()}
+                                            </Card.Title>
+                                        </Card.Body>
+                                    </Card>
+                                    <MDBMask style={{ width: '10rem', height: '15rem', fontSize: 15 }} overlay="black-strong">
+                                        <p className="white-text p-2 pt-10">DIRECTOR:<br />CAST:<br />RUNTIME:</p>
+                                    </MDBMask>
+                                </MDBView>
+                            </MDBCol>
+                        ))}
                     </MDBRow>
                 </MDBContainer>
             </div>
-        );
+        )
     }
 }
 
