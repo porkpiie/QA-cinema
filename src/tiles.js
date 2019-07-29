155dc74155bc8cd829e9ee60bb6096b0ee188226
gonsva <gonsva@gmail.com>
2019-07-29 11:31:40 +0100

    created tiles for new realses/upcoming

---
+++ src/tiles.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import image1 from './fid19112.jpg';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import { MDBMask, MDBView, MDBContainer, MDBRow, MDBCol } from "mdbreact";
+
+
+
+
+export default class Tiles extends React.Component{
+
+render(){
+    return(
+        <div>
+            
+            <MDBContainer>
+            <MDBRow>
+            <MDBCol>
+            <MDBView hover>
+
+                <Card bg="dark" text="white" style={{ width: '10rem' }}>        
+                    <Card.Img variant="top" src={image1} />
+                    <Card.Body className="p-2 pt-4">
+                    <Card.Title style={{textAlign:"left", fontSize:10}}>THE LION KING</Card.Title>
+                    </Card.Body>
+                </Card>
+            <MDBMask style={{ width: '10rem', height:'15rem', fontSize:15}} overlay="black-strong">
+            <p className="white-text p-2 pt-10">DIRECTOR:<br/>CAST:<br/>RUNTIME:</p>
+            </MDBMask>
+            </MDBView>
+
+            
+            </MDBCol>
+
+            <MDBCol>
+            <MDBView hover>
+
+                <Card bg="dark" text="white" style={{ width: '10rem' }}> 
+                    <Card.Img variant="top" src={image1} />
+                    <Card.Body className="p-2 pt-4">
+                    <Card.Title style={{textAlign:"left", fontSize:10}}>THE LION KING</Card.Title>
+                    </Card.Body>
+                    </Card>
+                    <MDBMask style={{ width: '10rem', height:'15rem', fontSize:15}} overlay="black-strong">
+            <p className="white-text p-2 pt-10">DIRECTOR:<br/>CAST:<br/>RUNTIME:</p>
+            </MDBMask>
+            </MDBView>
+            </MDBCol>
+
+            <MDBCol>
+            <MDBView hover>
+
+                <Card bg="dark" text="white" style={{ width: '10rem' }}> 
+                    <Card.Img variant="top" src={image1} />
+                    <Card.Body className="p-2 pt-4">
+                    <Card.Title style={{textAlign:"left", fontSize:10}}>THE LION KING</Card.Title>
+                    </Card.Body>
+                    </Card>
+                    <MDBMask style={{ width: '10rem', height:'15rem', fontSize:15}} overlay="black-strong">
+            <p className="white-text p-2 pt-10">DIRECTOR:<br/>CAST:<br/>RUNTIME:</p>
+            </MDBMask>
+            </MDBView>
+            </MDBCol>
+
+            <MDBCol>
+            <MDBView hover>
+
+                <Card bg="dark" text="white" style={{ width: '10rem' }}> 
+                    <Card.Img variant="top" src={image1} />
+                    <Card.Body className="p-2 pt-4">
+                    <Card.Title style={{textAlign:"left", fontSize:10}}>THE LION KING</Card.Title>
+                    </Card.Body>
+                    </Card>
+                    <MDBMask style={{ width: '10rem', height:'15rem', fontSize:15}} overlay="black-strong">
+            <p className="white-text p-2 pt-10">DIRECTOR:<br/>CAST:<br/>RUNTIME:</p>
+            </MDBMask>
+            </MDBView>
+            </MDBCol>
+            </MDBRow>
+            </MDBContainer>
+
+</div>
+    );
+}
+}
+
