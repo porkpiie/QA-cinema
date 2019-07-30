9ce7292467bd2a6248908770500595dda4194c47
porkpiie <adtready@gmail.com>
2019-07-30 12:05:17 +0100

    added carousel to home page

--- src/Components/Carousel.component.js
+++ src/Components/Carousel.component.js
@@ -1,27 +1,78 @@
-import React, { Component } from 'react';
-import { Carousel } from "react-responsive-carousel";
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+// import Carousel from 'react-bootstrap/Carousel'
+// import Carousel from 'react-bootstrap/Carousel'
+import imagea from "../Images/CarouselIMG.jpeg";
+import imageb from "../Images/toy-story-4.jpg";
+import imagec from "../Images/SpiderMan.jpg"
+import pg from "../Images/PG.png"
+import u from "../Images/U.png"
+import twelvea from "../Images/12A.png"
 
-export default class Carousel1 extends Component{
-  render(){
-  return(
-  <Carousel autoPlay>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg" alt="caro" />
-      <p className="legend">Legend 1</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-2.jpg" alt="caro"/>
-      <p className="legend">Legend 2</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-3.jpg" alt="caro"/>
-      <p className="legend">Legend 3</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-4.jpg" alt="caro"/>
-      <p className="legend">Legend 4</p>
-    </div>
-    </Carousel>
-);
-  }
+export default class CarouselHome extends React.Component {
+
+    render() {
+
+
+        return (
+            <div>
+
+                <div>
+
+                    <Carousel>
+                        <Carousel.Item>
+                            <div className="carousel-img-wrapper">
+                                <div className="imggradient" />
+                                <img
+                                    className="d-block w-100"
+                                    src={imagea}
+                                    alt="First slide"
+                                />
+                            </div>
+
+                            <Carousel.Caption>
+                                <h1 id="h1-carousel" style={{textAlign:"left", fontSize:40}}>THE LION KING</h1><br/>
+                                <img style={{ width: '1.5rem'}} align="left" src={pg} alt="p1" />
+                            </Carousel.Caption>
+                            
+                        </Carousel.Item>
+
+
+
+                        <Carousel.Item>
+                            <div className="carousel-img-wrapper">
+                                <div className="imggradient" />
+                                <img
+                                    className="d-block w-100"
+                                    src={imageb}
+                                    alt="Third slide"
+                                />
+                            </div>
+
+                            <Carousel.Caption>
+                            <h1 id="h1-carousel"style={{textAlign:"left", fontSize:40}}>TOY STORY 4</h1><br/>
+                            <img style={{ width: '1.5rem'}} align="left" src={u} alt="p2" />
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <div className="carousel-img-wrapper">
+                                <div className="imggradient" />
+                                <img
+                                    className="d-block w-100"
+                                    src={imagec}
+                                    alt="Third slide"
+                                />
+                            </div>
+
+                            <Carousel.Caption>
+                                <h1 id="h1-carousel"style={{textAlign:"left", fontSize:40}}>SPIDER-MAN: FAR FROM HOME</h1><br/>
+                                <img style={{ width: '1.5rem'}} align="left" src={twelvea} alt="p3" />
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
+            </div>
+
+        );
+    }
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,36 +1,13 @@
 import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import CarouselHome from '../Components/Carousel.component';
 
 
 export default class Home extends Component{
     render(){
         return(
             <div>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
-            <h1>Testing Testing 123</h1>
+            <CarouselHome />
             
             </div>
         )
