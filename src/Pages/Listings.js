6132da9fc2c876ea8948778eaa2d6ce3c24ffcbd
gonsva <gonsva@gmail.com>
2019-08-05 14:53:39 +0100

    updated styling

--- src/Pages/Listings.js
+++ src/Pages/Listings.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import Tiles from '../Components/Tiles.components';
 import Banner from '../Components/Banner.component';
+import { Container } from "react-bootstrap";
 
 export default class Listings extends Component{
     render(){
         return(
             <div>
+            <div className="banner-img-wrapper">
                 <Banner />
+            </div>
+
+            <Container className="homecontainer">
             <h1 className="Listingh1">NOW SHOWING</h1>
             <hr/>
-            
             <Tiles/>
+            </Container>
             </div>
         )
     }
--- src/Pages/NewReleases.js
+++ src/Pages/NewReleases.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import Tiles from '../Components/Tiles.components';
-import Whale from '../Images/whalebanner.png';
+import Banner from '../Components/Banner.component';
+import { Container } from "react-bootstrap";
 
 export default class Listings extends Component{
     render(){
         return(
             <div>
-                <img src={Whale} width="100%" height="5%" alt="whaleban" />
+            <div className="banner-img-wrapper">
+                <Banner />
+            </div>
+            <Container className="homecontainer">
             <h1 className="Listingh1">COMING SOON</h1>
             <hr/>
-            
             <Tiles/>
+            </Container>
             </div>
         )
     }
