b5bb973caa3645edb554a039a5951ab1ba8d51a5
porkpiie <adtready@gmail.com>
2019-08-07 14:42:45 +0100

    scrum description updated

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -20,6 +20,8 @@ export default class AboutUs extends Component {
                     <hr />
                     <h1 className="Listingh1">OUR JOURNEY USING <a href="https://www.scrum.org/" target="__blank" style={{ color: "#28CDE9" }}>SCRUM</a></h1>
                     <hr />
+                    <p id="Carouselp1"> Scrum is a framework that helps teams work together. Much like a rugby team (where it gets its name) training for the big game,
+                    Scrum encourages teams to learn through experiences, self-organize while working on a problem, and reflect on their wins and losses to continuously improve.</p> 
                     <p id="Carouselp1">As a team we have used Scrum during this project.</p>
                     <p id="Carouselp1">Every day we have had a stand up to see what everyone is working on & if they have any blockers to the tasks.</p>
                     <p id="Carouselp1">We have assigned stories to our sprint each week and have been able to be flexible on our workload because of this.</p><br/>
