5c91aa5c4f24384414244a5bd965fd532ef717e6
DanielAShurmer <thatblackshadowcat@outlook.com>
2019-08-07 12:45:16 +0100

    Successful Bookings Now Send Confirmation Emails

--- src/Components/Payment.component.js
+++ src/Components/Payment.component.js
@@ -7,7 +7,8 @@ class CheckoutForm extends Component {
 		this.state = {
 			complete: false,
 			validBooking: false,
-			booking: []
+			booking: [],
+			bookingID: ""
 		};
 		this.submit = this.submit.bind(this);
 	}
@@ -22,7 +23,6 @@ class CheckoutForm extends Component {
 	}
 
 	async submit(ev) {
-		let { token } = await this.props.stripe.createToken({ name: "Name" });
 
 		let booking = this.state.booking;
 		booking = JSON.stringify(booking);
@@ -36,58 +36,74 @@ class CheckoutForm extends Component {
 			}
 		}).then(response => {
 			response.text().then(data => {
-				console.log(data);
+				data = JSON.parse(data);
+				this.state.bookingID = data.id;
 				sessionStorage.removeItem("bookingData");
 				this.setState({ complete: true });
+
+				let request = {};
+				request.bookingID = this.state.bookingID;
+				request = JSON.stringify(request);
+			
+				fetch("http://localhost:8080/qac/payment/", {
+					method: "POST",
+					body: request,
+					headers: {
+						Accept: "application/json",
+						"Content-Type": "application/json"
+					}
+				})
 			});
 		});
+	
 	}
-
 	render() {
-		if (this.state.validBooking) {
-			if (this.state.complete) {
-				console.log(this.state.booking);
-				let filmName = "";
-				for (let character in this.state.booking.filmName) {
-					if (this.state.booking.filmName[character] === "_"){
-						filmName += " ";
+					if(this.state.validBooking) {
+				if (this.state.complete) {
+					console.log(this.state.booking);
+					let filmName = "";
+					for (let character in this.state.booking.filmName) {
+						if (this.state.booking.filmName[character] === "_") {
+							filmName += " ";
+						}
+						else {
+							filmName += this.state.booking.filmName[character];
+						}
 					}
-					else {
-						filmName += this.state.booking.filmName[character];
-					}
-				}
-				return (
-					<div>
-						<h1 style={{ color: 'black', textAlign: "center" }}>Purchase Complete</h1>
-						<h3> Thank You For Your Purchase {this.state.booking.firstName}</h3>
-						<h4> You Have Booked To See {filmName + " "}
-						 On {this.state.booking.filmDate + " "}  At {this.state.booking.filmTime}</h4>
-						<h5> A Confirmation Email Will Be Sent To {this.state.booking.email} Shortly</h5>
-					</div>
-				);
-			}
-
-			else {
-				return (
-					<div className="checkout">
-						<p>Would you like to complete the purchase?</p>
-						<div style={{ color: 'black', textAlign: "center", width: '30%' }}>
-							<CardElement />
+					return (
+						<div>
+							<h1 style={{ color: 'black', textAlign: "center" }}>Purchase Complete</h1>
+							<h3> Thank You For Your Purchase {this.state.booking.firstName}</h3>
+							<h3> Your Booking ID Is {this.state.bookingID}</h3>
+							<h3> Please Retain This Number And Bring It To Your Film Showing</h3>
+							<h4> You Have Booked To See {filmName + " "}
+								On {this.state.booking.filmDate + " "}  At {this.state.booking.filmTime}</h4>
+							<h5> A Confirmation Email Will Be Sent To {this.state.booking.email} Shortly</h5>
 						</div>
-						<button onClick={this.submit}>Send</button>
-					</div>
+					);
+				}
+
+				else {
+					return (
+						<div className="checkout">
+							<p>Would you like to complete the purchase?</p>
+							<div style={{ color: 'black', textAlign: "center", width: '30%' }}>
+								<CardElement />
+							</div>
+							<button onClick={this.submit}>Send</button>
+						</div>
+					);
+				}
+			}
+		else {
+				return (
+					<h1 style={{ color: 'black', textAlign: "center" }}>No In-Progess Booking Found</h1>
 				);
 			}
 		}
-		else {
-			return (
-				<h1 style={{ color: 'black', textAlign: "center" }}>No In-Progess Booking Found</h1>
-			);
-		}
-	}
 }
 
-export default injectStripe(CheckoutForm);
+	export default injectStripe(CheckoutForm);
 
 
 
